bc223de7d8d79d0a794fc171047301386abf5d08
Ian-Ag-96 <agumeian96@gmail.com>
2025-02-16 14:30:06 +0300

    Modified sql file

--- card-test.sql
+++ card-test.sql
@@ -1,12 +1,14 @@
+--
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.6
--- Dumped by pg_dump version 16.6
+-- Dumped from database version 17.2
+-- Dumped by pg_dump version 17.2
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -35,6 +37,7 @@ CREATE TABLE public.cards (
     CONSTRAINT status_check CHECK (((status)::text = ANY (ARRAY[('To Do'::character varying)::text, ('In Progress'::character varying)::text, ('Done'::character varying)::text])))
 );
 
+
 ALTER TABLE public.cards OWNER TO postgres;
 
 --
@@ -49,6 +52,7 @@ CREATE TABLE public.users (
     CONSTRAINT users_role_check CHECK (((role)::text = ANY (ARRAY[('Member'::character varying)::text, ('Admin'::character varying)::text])))
 );
 
+
 ALTER TABLE public.users OWNER TO postgres;
 
 --
@@ -56,7 +60,11 @@ ALTER TABLE public.users OWNER TO postgres;
 --
 
 COPY public.cards (name, description, color, status, date_created, creator, is_active) FROM stdin;
+CardOne			To Do	2025-02-15 17:43:57.823473	adminone@administrators.com	Y
+CardTwo_deleted_2025-02-15T18:40:15.781901100	Currently being processed.	#000000	In Progress	2025-02-15 18:09:50.010639	adminone@administrators.com	N
+CardFourMember_deleted_2025-02-15T18:50:32.151308400			To Do	2025-02-15 18:45:25.425532	memberthree@allmembers.com	N
 \.
+
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -65,7 +73,10 @@ COPY public.cards (name, description, color, status, date_created, creator, is_a
 COPY public.users (email, password, role, date_created) FROM stdin;
 adminone@administrators.com	$2a$10$5M44l1L2MPnM1bP7bH3dLuiEVkgkkx6m7il.TRNkoAuxMRxrmWmxm	Admin	2025-02-13 11:47:54.60513
 memberone@allmembers.com	$2a$10$axVkI2YaJWHsO6ZowODvauj2/F0SQJozz1QK91mHKCq/rcyTBW0ga	Member	2025-02-13 11:55:42.605248
+membertwo@allmembers.com	$2a$10$WcnRRT9J1JNFkqK5TcZdW.ub4kyFt8R4avOOKnYGsZs1a0cLu6pje	Member	2025-02-15 16:26:08.650672
+memberthree@allmembers.com	$2a$10$FX/AYqrAB24jSOuka4u7Z.4L38mYPzHow41dIEK3kjCeDP8zpycTi	Member	2025-02-15 16:45:02.998876
 \.
+
 
 --
 -- Name: cards cards_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
@@ -74,6 +85,7 @@ memberone@allmembers.com	$2a$10$axVkI2YaJWHsO6ZowODvauj2/F0SQJozz1QK91mHKCq/rcyT
 ALTER TABLE ONLY public.cards
     ADD CONSTRAINT cards_pkey PRIMARY KEY (name);
 
+
 --
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
@@ -81,6 +93,8 @@ ALTER TABLE ONLY public.cards
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (email);
 
+
 --
 -- PostgreSQL database dump complete
 --
+
