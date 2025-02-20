6ed6c3b03d1e987a6b828bf18f83cca114e98e92
Ian-Ag-96 <agumeian96@gmail.com>
2025-02-20 08:05:42 +0300

    Minor change to docker config and testConn api.

--- src/main/java/com/eclecticsassignment/cards/dbconn/DBConn.java
+++ src/main/java/com/eclecticsassignment/cards/dbconn/DBConn.java
@@ -11,11 +11,16 @@ import lombok.extern.slf4j.Slf4j;
 public class DBConn {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/card_test";
+    private static final String DOCKER_URL = "jdbc:postgresql://postgres:5432/card_test";
     private static final String USER = "postgres";
     private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    
+    public static Connection getConnectionDocker() throws SQLException {
+        return DriverManager.getConnection(DOCKER_URL, USER, PASSWORD);
     }
 
     public static String testConn() {
@@ -28,7 +33,17 @@ public class DBConn {
                 res = "Success";
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+        	log.info("Failed to connect using local URL. Using docker URL...");
+        	try (Connection conn2 = DBConn.getConnectionDocker()) {
+                if (conn2 != null) {
+                	log.info("Conn URL: {}", conn2.getMetaData().getURL());
+                	log.info("Conn User: {}", conn2.getMetaData().getUserName());
+                    System.out.println("Connected to the database successfully!");
+                    res = "Success";
+                }
+            } catch (SQLException e2) {
+            	e2.printStackTrace();
+            }
         }
         return res;
     }
